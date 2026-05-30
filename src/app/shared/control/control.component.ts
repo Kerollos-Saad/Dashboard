265471f313cd2b4cced80f41851947d7b909b084
Kerollos-Saad <kirloussaad7788@gmail.com>
2026-05-31 02:10:23 +0300

    .

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, contentChild, ContentChild, ElementRef, input, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, contentChild, ContentChild, ElementRef, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -8,11 +8,16 @@ import { Component, contentChild, ContentChild, ElementRef, input, ViewEncapsula
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
 })
-export class ControlComponent {
+export class ControlComponent implements AfterViewInit{
 
   @ContentChild('input') private control?: ElementRef<HTMLInputElement | HTMLTextAreaElement>; // non-signal
 
   private control2 = contentChild<ElementRef<HTMLInputElement | HTMLTextAreaElement>>('input'); // signal
+
+  ngAfterViewInit(){
+    console.log(this.control);
+    console.log(this.control2);
+  }
 
   title = input.required<string>();
 }
